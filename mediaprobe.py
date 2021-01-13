1c4402ae22d655621efa1d0fdb98c482699fa374
Eman <eacosta@roundabout.com>
2021-01-13 14:27:52 -0800

    Added docstrings
    Changed getaudio to return total number of channels with the option of returning stream index/channels per stream. Also added the option of returning the raw bytes from mediainfo in the getall function

--- mediaprobe.py
+++ mediaprobe.py
@@ -1,4 +1,10 @@
+"""
+A straightforward wrapper for the Mediainfo CLI tool. It calls mediainfo as a subprocess and parses the returning
+JSON into formatted datatypes relevent to the function called.
+"""
+
 import json
+from json import encoder
 import pathlib
 import subprocess as sub
 from sys import platform
@@ -11,16 +17,32 @@ def _init():
 
     if platform == "win32":
         mibin = pathlib.Path("./bin/mediainfo.exe")
+    else:
+        raise EnvironmentError("This library is currently only compatible with Windows")
 
-def getall(filepath):
+def getall(filepath, raw=False):
+    """
+    Returns a dictionary with 2 keys: 'path' and 'tracks'. 'path' is the original path
+    that was passed into this function. 'tracks' contains a list of tracks where each
+    track is a dictionary that contains properties for that track.
+
+    Setting 'raw' to 'True' will return the unchanged JSON output(as bytes) straight from the Mediainfo CLI.
+    """
     if not pathlib.Path(filepath).is_file():
-        raise TypeError(f"'{filepath}' is not a path to a file or does not exist")
+        raise FileNotFoundError(f"'{filepath}' is not a path to a file or does not exist")
     try:
         mibin
     except NameError:
         _init()
     miproc = sub.Popen(f"{str(mibin)} {filepath} --output=JSON", stdin=sub.PIPE, stdout=sub.PIPE, stderr=sub.PIPE)
-    fileoutput = json.loads(miproc.stdout.read())['media']
+    rawbytes = miproc.stdout.read()
+    rawstr = rawbytes.decode('utf-8')
+    fileoutput = json.loads(rawstr)
+
+    if raw:
+        return raw
+
+    fileoutput = fileoutput['media']
 
     path = fileoutput['@ref']
     del fileoutput['@ref']
@@ -30,9 +52,16 @@ def getall(filepath):
     del fileoutput['track']
     fileoutput['tracks'] = tracks
 
-    return fileoutput
+
+    return dict(fileoutput)
 
 def getaudio(filepath, tracks=False):
+    """
+    Returns the total number of audio channels as an int. 
+
+    If 'tracks=True', it instead returns a list of tuples.
+    The tuples contain the index of the stream and the number of channels in that stream.
+    """
     output = getall(filepath)
     
     chspertrack = []
@@ -44,17 +73,35 @@ def getaudio(filepath, tracks=False):
             trackorder.append(track['StreamOrder'])
 
     if tracks:
-        return [x for x in zip(trackorder, chspertrack)]
+        return list([x for x in zip(trackorder, chspertrack)])
     else:
         totalchs = 0
         for ch in chspertrack:
             totalchs += int(ch)
-        return totalchs
+        return int(totalchs)
     
 def getfps(filepath):
     output = getall(filepath)
 
     for track in output['tracks']:
         if track['@type'] == "Video":
-            return track['FrameRate']
+            return str(track['FrameRate'])
+
+def getstreamtypes(filepath):
+    output = getall(filepath)
+
+    alltypes = []
+    order = []
+
+    for track in output['tracks']:
+        if track['@type'] != 'General':
+            alltypes.append(track['@type'])
+            order.append(track['StreamOrder'])
+
+    tosort = [x for x in zip(alltypes, order)]
+    tosort.sort(key= lambda x:x[1])
+    sorted = [x[0] for x in tosort]
+    
+    return sorted
+
 
