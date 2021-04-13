3ceb514f695a0d81a85700df754111b6f728bb07
Emmanuel Acosta <eacosta@roundabout.com>
2021-04-12 17:50:02 -0700

    Added additional checks for module/freezing imports

--- mediaprobe/__init__.py
+++ mediaprobe/__init__.py
@@ -1,1 +1,1 @@
-from mediaprobe.main import *
+from .main import *
--- mediaprobe/main.py
+++ mediaprobe/main.py
@@ -6,17 +6,13 @@ JSON into formatted datatypes relevent to the function called.
 import json
 import pathlib
 import subprocess as sub
-import sys
+from sys import platform
 
+testfile = pathlib.Path('c:', '/Mount', 'rei08', 'DCI', 'testing', '_Testfiles', 'Logan_8Ch.mov')
 here = pathlib.Path(__file__).parent.resolve()
-platform = sys.platform
 
 if platform == "win32":
     mibin = here / 'bin' / 'mediainfo.exe'
-    if not mibin.is_file():
-    	mibin = here.parent.resolve() / 'bin' / 'mediainfo.exe'
-    if not mibin.is_file():
-        raise EnvironmentError("Unable to locate the MediaInfo exe")
 else:
     raise EnvironmentError("This library is currently only compatible with Windows")
 
@@ -31,6 +27,9 @@ def all(filepath, raw=False):
     if not pathlib.Path(filepath).is_file():
         raise FileNotFoundError(f"'{filepath}' is not a path to a file or does not exist")
 
+    if " " in str(filepath):
+        filepath = f'"{str(filepath)}"'
+
     miproc = sub.Popen(f"{str(mibin)} {filepath} --output=JSON", stdin=sub.PIPE, stdout=sub.PIPE, stderr=sub.PIPE)
     rawbytes = miproc.stdout.read()
     rawstr = rawbytes.decode('utf-8')
@@ -39,10 +38,7 @@ def all(filepath, raw=False):
     if raw:
         return rawbytes
 
-    try:
-        fileoutput = fileoutput['media']
-    except KeyError:
-        return False
+    fileoutput = fileoutput['media']
 
     path = fileoutput['@ref']
     del fileoutput['@ref']
@@ -52,27 +48,31 @@ def all(filepath, raw=False):
     del fileoutput['track']
     fileoutput['tracks'] = tracks
 
+
     return dict(fileoutput)
 
-def audio(filepath, tracks=False):
+def audio(filepath, tracks=False, pids=False):
     """
     Returns the total number of audio channels as an int. 
 
     If 'tracks=True', it instead returns a list of tuples.
     The tuples contain the index of the stream and the number of channels in that stream.
+    If 'pids=True', it returns the pids in the first pos of the tuple instead of the stream order.
     """
     output = all(filepath)
-
-    if not output:
-    	return False
     
     chspertrack = []
     trackorder = []
 
+    if pids:
+        datafield = 'ID'
+    else:
+        datafield = 'StreamOrder'
+
     for track in output['tracks']:
         if track["@type"] == 'Audio':
             chspertrack.append(track['Channels'])
-            trackorder.append(track['StreamOrder'])
+            trackorder.append(track[datafield])
 
     if tracks:
         return list([x for x in zip(trackorder, chspertrack)])
@@ -81,84 +81,19 @@ def audio(filepath, tracks=False):
         for ch in chspertrack:
             totalchs += int(ch)
         return int(totalchs)
-
-def findaudiostream(filepath, chnum):
-    """
-    Returns the stream number for the provided audio. E.g. if the source file has 4 streams in this order:
-    Video, Data, 5.1, Stereo - Chs 1-6 would return "2", and 7-8 would return "3".
-    """
-    if type(chnum) != int:
-        raise TypeError(f"chnum parameter must be int not {type(chnum)}")
-    elif chnum <= 0:
-        raise ValueError(f"chnum must be greater than 0. Got: {chnum}")
-
-    totalchs = audio(filepath, False)
-    audiostreams = audio(filepath, True)
-
-    if not totalchs or not audiostreams:
-        return False
-
-    if chnum > totalchs:
-        raise ValueError(f"chnum is greater than the total number of channels. chnum: {chnum} , total: {totalchs}")
-
-    chcount = 0
-    streamindex = False
-    for stream in audiostreams:
-        chcount += int(stream[1])
-        if chcount >= chnum:
-            streamindex = stream[0]
-            break
-    return str(streamindex)
     
 def fps(filepath):
     output = all(filepath)
 
-    if not output:
-    	return False
-
     for track in output['tracks']:
         if track['@type'] == "Video":
             return str(track['FrameRate'])
-
-    return False
-
-def tc(filepath):
-    """
-    Looks for the starting TC in the "Other" track type, under the TimeCode_FirstFrame field.
-    If it can't find it, it returns False.
-    """
-    output = all(filepath)
-    if not output:
-    	return False
-
-    try:
-        tracks = output["tracks"]
-    except KeyError:
-        return False
-    
-    othertrk = None
-    for trk in tracks:
-        if trk["@type"] == "Other":
-            othertrk = trk
-
-    if othertrk == None:
-        return False
-
-    try:
-        tc = othertrk["TimeCode_FirstFrame"]
-    except KeyError:
-        return False
-    else:
-        return str(tc)
-
 
 def streamtypes(filepath):
     """
     Returns a list that contains types for each stream, in order.
     """
     output = all(filepath)
-    if not output:
-    	return False
 
     alltypes = []
     order = []
@@ -173,7 +108,3 @@ def streamtypes(filepath):
     sorted = [x[0] for x in tosort]
     
     return sorted
-
-if __name__ == "__main__":
-    testfile = pathlib.Path(r"C:\Mount\rei08\encoding\_FFMPEG_CC_PROXY\_Old\Other\2997_ASM_NDF\Input\gbi_00_final_txtd_178_HQ_20_2997_NDF_wf.mov")
-    print(findaudiostream(testfile, 0))
