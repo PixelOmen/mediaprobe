62b0ca44c64fd1d60c9f6a691555176a5c0f1504
Emmanuel Acosta <eacosta@roundabout.com>
2022-03-23 11:54:14 -0700

    Removed unneeded files in master branch

--- scratch.py
+++
@@ -1,7 +0,0 @@
-from mediaprobe import MediaProbe, TESTFILE
-
-longtest = r"\\10.0.20.175\rei08\_Andy\DisneyFX\KingOfTheHill_s01\deliverable\KingOfTheHill_1ABE01_XXKH01001.mxf"
-
-probe = MediaProbe(TESTFILE)
-
-print(probe.audio())
